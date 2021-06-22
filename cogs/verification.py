d8316f070244d919074c36b523261d9f078a72b6
FrankMerin <61675381+FrankMerin@users.noreply.github.com>
2021-06-22 18:24:23 -0400

    updated greeting

--- cogs/verification.py
+++ cogs/verification.py
@@ -112,7 +112,13 @@ class verification(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        await member.send('If you would like to gain access to the verified students channel, please provide your @baruchmail.cuny.edu email for verification.')
+        await member.send('''**Welcome to the Baruch College Discord Server!**
+ 
+To gain access to the server, please visit #role-assignment channel and select a role.
+
+Additionally, to protect against spam, you must have an email attached to your discord account to talk in the server.
+
+**Optional:** If you would like to gain access to the verified students channel, please provide your @baruchmail.cuny.edu email for verification.''')
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
