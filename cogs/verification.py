2ea58ce5a0cc4231417f3988a6c7d0b447d3a964
Frank Merin <61675381+FrankMerin@users.noreply.github.com>
2022-02-21 15:04:56 -0500

    separating on join

--- cogs/verification.py
+++ cogs/verification.py
@@ -110,16 +110,7 @@ class verification(commands.Cog):
         self.verified_role = [role for role in self.guild.roles if role.name == 'Verified'][0]
         self.verified_channel = [channel for channel in self.guild.channels if channel.name == 'verification-log'][0]
 
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        await member.send('''**Welcome to the Baruch College Discord Server!**
- 
-To gain access to the server, please visit #role-assignment channel and select a role.
-
-Additionally, to protect against spam, you must have an email attached to your discord account to talk in the server.
-
-**Optional:** If you would like to gain access to the verified students channel, please provide your @baruchmail.cuny.edu email for verification.''')
-
+   
     @commands.Cog.listener()
     async def on_message(self, ctx):
         if ctx.author.bot:
